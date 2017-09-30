4746347bedc1e9287f03a5dfe5d3fdbeb22da4ae
Lazaro <cuervo.piedritas@gmail.com>
2017-09-30 17:38:08 -0300

    Add a custom exception class

---
+++ SenseFilms/common/sensefilms/exceptions/LoggedException.java
@@ -0,0 +1,18 @@
+package sensefilms.exceptions;
+
+public class LoggedException extends Exception
+{
+
+	private static final long serialVersionUID = 1L;
+	
+	public LoggedException() 
+	{
+		super();
+	}
+	
+	public LoggedException(String message) 
+	{
+		super(message);
+	}
+
+}
