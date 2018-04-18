fd35c8c2bee6ddd6e325bc113a56faab29db4b8f
lazaro94 <cuervo.piedritas@gmail.com>
2018-04-17 23:58:14 -0300

    [Refactor] Move random string method to string utils class

--- SenseFilms.MVC/src/main/java/com/sensefilms/common/utils/StringUtils.java
+++ SenseFilms.MVC/src/main/java/com/sensefilms/common/utils/StringUtils.java
@@ -1,10 +1,17 @@
 package com.sensefilms.common.utils;
 
+import java.util.UUID;
+
 public class StringUtils 
 {
 	public static boolean isNullorEmpty(String input) 
 	{
 		return (input == null || input.isEmpty());
+	}
+	
+	public static String getRandomStringBasedOnGuid(int stringLength) 
+	{
+		return UUID.randomUUID().toString().toLowerCase().replace("-", "").substring(0, stringLength);
 	}
 	
 	public static final String EMPTY = "";
--- SenseFilms.MVC/src/main/java/com/sensefilms/services/implementation/AccountService.java
+++ SenseFilms.MVC/src/main/java/com/sensefilms/services/implementation/AccountService.java
@@ -21,148 +21,147 @@ import com.sensefilms.services.base.BaseService;
 import com.sensefilms.services.contracts.IAccountService;
 
 @Service
-public class AccountService extends BaseService implements IAccountService 
+public class AccountService extends BaseService implements IAccountService
 {
 	private IMailHandler mailHandler;
 	private IAuditHandler auditHandler;
 	private static HashMap<String, User> authenticatedUsers = new HashMap<String, User>();
-	
+
 	@Autowired
-	public AccountService(IUserRepository userRepository, IMailHandler mailHandler, IAuditHandler auditHandler) 
+	public AccountService(IUserRepository userRepository, IMailHandler mailHandler, IAuditHandler auditHandler)
 	{
 		super(AccountService.class);
 		this._userRepository = userRepository;
 		this.mailHandler = mailHandler;
 		this.auditHandler = auditHandler;
 	}
-	
+
 	private IUserRepository _userRepository;
-	
+
 	@Override
-	public void tryAuthenticateUser(User user) throws CustomBusinessException, CustomHandledException 
+	public void tryAuthenticateUser(User user) throws CustomBusinessException, CustomHandledException
 	{
-		try 
+		try
 		{
 			User dbUser = _userRepository.getOneByUsername(user.getUsername());
-			
-			if(dbUser == null || !dbUser.getPassword().equals(user.getPassword()))
+
+			if (dbUser == null || !dbUser.getPassword().equals(user.getPassword()))
 				throw new CustomBusinessException("Incorrect credentials, please try again.");
-					
-				dbUser.setLastLogin(new Date());
-				_userRepository.update(dbUser);
-				addUserToCache(dbUser);					
-		}
-		catch(CustomBusinessException cbEx) 
+
+			dbUser.setLastLogin(new Date());
+			_userRepository.update(dbUser);
+			addUserToCache(dbUser);
+		} 
+		catch (CustomBusinessException cbEx)
 		{
 			throw cbEx;
-		}
-		catch(HibernateException hex)
+		} 
+		catch (HibernateException hex)
 		{
 			throw new CustomHandledException("An error ocurred when try to access the database.", hex);
-		}
-		catch(Exception ex)
-		{
-			throw new CustomHandledException("A totally unexpected error occurred. Please run as fast as you can!", ex);
-		}
-	}
-	
-	@Override
-	public void updateNewPassord(String username, String newPassword) throws CustomBusinessException, CustomHandledException
-	{
-		User currentUser = null;
-		boolean isRecoveryProcess = StringUtils.isNullorEmpty(newPassword);
-		
-		try 
-		{
-			currentUser = _userRepository.getOneByUsername(username);
-			
-			if(currentUser == null) 
-				throw new CustomBusinessException("The username doesn't exists.");
-			
-			//Generate a 10 chars random password based on a Guid
-			if(isRecoveryProcess)
-				newPassword = UUID.randomUUID().toString().toLowerCase().replace("-", "").substring(0, 10);
-							 
-			currentUser.setPassword(newPassword);
-			currentUser.setNewPassword(isRecoveryProcess);
-			_userRepository.update(currentUser);
-			getLogger().debug(String.format("Password regenerated for user %s", username));
-			
-			//Send an email with the temporary password
-			if(isRecoveryProcess)
-				sendEmailWithNewPassword(newPassword, currentUser.getEmail());
-			
-			auditPasswordChange(isRecoveryProcess, currentUser.getUsername());
-		}
-		catch(CustomBusinessException cbEx) 
-		{
-			throw cbEx;
-		}
-		catch(HibernateException hex)
-		{
-			throw new CustomHandledException("An error ocurred when try to access the database.", hex);
-		}
-		catch(MessagingException msgEx) 
-		{
-			throw new CustomHandledException(String.format("An error ocurred when try send an email to the addres %s.", currentUser.getEmail()), msgEx);	
-		}
-		catch(Exception ex)
+		} 
+		catch (Exception ex)
 		{
 			throw new CustomHandledException("A totally unexpected error occurred. Please run as fast as you can!", ex);
 		}
 	}
 
 	@Override
-	public void addNewUser(User user) throws CustomHandledException 
+	public void updateNewPassord(String username, String newPassword) throws CustomBusinessException, CustomHandledException
 	{
+		User currentUser = null;
+		boolean isRecoveryProcess = StringUtils.isNullorEmpty(newPassword);
+
 		try
 		{
-			this._userRepository.insert(user);
-		}
-		catch(HibernateException hex)
+			currentUser = _userRepository.getOneByUsername(username);
+
+			if (currentUser == null)
+				throw new CustomBusinessException("The username doesn't exists.");
+
+			if (isRecoveryProcess)
+				newPassword = StringUtils.getRandomStringBasedOnGuid(10);
+
+			currentUser.setPassword(newPassword);
+			currentUser.setNewPassword(isRecoveryProcess);
+			_userRepository.update(currentUser);
+			getLogger().debug(String.format("Password regenerated for user %s", username));
+
+			// Send an email with the temporary password
+			if (isRecoveryProcess)
+				sendEmailWithNewPassword(newPassword, currentUser.getEmail());
+
+			auditPasswordChange(isRecoveryProcess, currentUser.getUsername());
+		} 
+		catch (CustomBusinessException cbEx)
+		{
+			throw cbEx;
+		} 
+		catch (HibernateException hex)
 		{
 			throw new CustomHandledException("An error ocurred when try to access the database.", hex);
-		}
-		catch(Exception ex)
+		} 
+		catch (MessagingException msgEx)
+		{
+			throw new CustomHandledException(String.format("An error ocurred when try send an email to the addres %s.", currentUser.getEmail()), msgEx);
+		} 
+		catch (Exception ex)
 		{
 			throw new CustomHandledException("A totally unexpected error occurred. Please run as fast as you can!", ex);
 		}
 	}
-	
+
+	@Override
+	public void addNewUser(User user) throws CustomHandledException
+	{
+		try
+		{
+			this._userRepository.insert(user);
+		} 
+		catch (HibernateException hex)
+		{
+			throw new CustomHandledException("An error ocurred when try to access the database.", hex);
+		} 
+		catch (Exception ex)
+		{
+			throw new CustomHandledException("A totally unexpected error occurred. Please run as fast as you can!", ex);
+		}
+	}
+
 	public static User getAuthenticatedUserByUsername(String username)
 	{
 		return authenticatedUsers.get(username);
 	}
-	
+
 	// PRIVATE HELPERS METHODS //
-	private void addUserToCache(User user) 
+	private void addUserToCache(User user)
 	{
-		if(!authenticatedUsers.containsKey(user.getUsername()))
+		if (!authenticatedUsers.containsKey(user.getUsername()))
 			authenticatedUsers.remove(user.getUsername());
-		
+
 		authenticatedUsers.put(user.getUsername(), user);
 	}
-	
-	private void auditPasswordChange(boolean isRecoveryProcess, String username) 
+
+	private void auditPasswordChange(boolean isRecoveryProcess, String username)
 	{
 		String eventDescription = isRecoveryProcess ? String.format("Random password generated for user %s", username)
-				: String.format("Password updated for user %s", username);
-		
+					: String.format("Password updated for user %s", username);
+
 		this.auditHandler.handleNewAuditEvent("[Update-Password]", eventDescription, StringUtils.EMPTY);
 	}
-	
-	private void sendEmailWithNewPassword(String randomPassword, String email) throws MessagingException 
+
+	private void sendEmailWithNewPassword(String randomPassword, String email) throws MessagingException
 	{
 		String emailBodyText = String.format("This is your new password \"%s\". ", randomPassword);
-		try 
+		try
 		{
 			mailHandler.sendMailMessage(email, "Password Recover", emailBodyText);
 		} 
-		catch (MessagingException msgEx) 
+		catch (MessagingException msgEx)
 		{
 			throw msgEx;
-		}		
+		}
 		getLogger().debug(String.format("Email send to %s succesfully.", email));
 	}
-	
+
 }
